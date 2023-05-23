ef7b4e4689b504ac473b8a075f0dfd9652cd3507
Xin Su <julian.x.su@gmail.com>
2023-05-23 13:44:37 -0400

    pytest001

---
+++ Jour_4/test_premierTest.py
@@ -0,0 +1,8 @@
+# 1: Installer "pytest" ==> pip install pytest
+# 2：Creer un fichier python ==> Doit contenir le mot "test" au debut
+# 3: Importer le package "pytest"
+
+import pytest
+
+def test_login():
+    print("ce connecter sur google")
